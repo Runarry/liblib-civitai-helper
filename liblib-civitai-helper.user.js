c4aeb35c52406eb9bc7edc85739c57e0561a10e2
Runarry <sleep.rotten.banana@hotmail.com>
2025-04-23 16:27:23 +0800

    ✨ feat(liblib-civitai-helper): 支持http协议的匹配规则
    - 新增对http协议下liblib.ai和liblib.art模型信息页的匹配支持
    - 兼容更多访问场景，提升脚本适用性

--- liblib-civitai-helper.user.js
+++ liblib-civitai-helper.user.js
@@ -5,6 +5,8 @@
 // @description  liblib|civitai助手，支持自动保存到目录（Chromium），或自动批量下载（Firefox/Safari等），封面图片和json同名，兼容新版Civitai接口和页面
 // @match        https://www.liblib.ai/modelinfo/*
 // @match        https://www.liblib.art/modelinfo/*
+// @match        http://www.liblib.ai/modelinfo/*
+// @match        http://www.liblib.art/modelinfo/*
 // @match        https://civitai.com/models/*
 // @match        http://civitai.com/models/*
 // @updateURL    https://raw.githubusercontent.com/Runarry/liblib-civitai-helper/refs/heads/main/liblib-civitai-helper.user.js
